e51311ca354e5f353389cb1341460daca5121867
irving <irving1364@gmail.com>
2024-05-04 22:36:29 -0400

    Actualización

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,13 +3,7 @@ import Wrapper from "@layout/wrapper";
 import Header from "@layout/header/header-01";
 import Footer from "@layout/footer/footer-01";
 import HeroArea from "@containers/hero/layout-09";
-import TopSellerArea from "@containers/top-seller/layout-02";
-import AboutArea from "@containers/about/layout-01";
-import LiveExploreArea from "@containers/live-explore/layout-01";
-import ProductArea from "@containers/product/layout-01";
-import ProductAreaTwo from "@containers/product/layout-02";
-import ServiceArea from "@containers/services/layout-01";
-import CollectionArea from "@containers/collection/layout-01";
+
 import { normalizedData } from "@utils/methods";
 
 
@@ -18,10 +12,7 @@ import ExploreProductArea from "@containers/explore-product/layout-01";
 
 // Demo data
 import homepageData from "../data/homepages/home-09.json";
-import sellerData from "../data/sellers.json";
 import productData from "../data/products-02.json";
-import collectionsData from "../data/collections.json";
-import notificationData from "../data/notifications.json";
 
 export async function getStaticProps() {
     return { props: { className: "template-color-1" } };
@@ -50,36 +41,6 @@ const Home09 = () => {
                     }}
                 />
 
-                {/* 
-                <TopSellerArea data={{ sellers: sellerData.slice(0, 6) }} />
-                <AboutArea data={content["about-section"]} />
-                <LiveExploreArea
-                    data={{
-                        ...content["live-explore-section"],
-                        products: liveAuctionData,
-                    }}
-                />
-                <ProductArea
-                    data={{
-                        ...content["product-section"],
-                        products: productData,
-                        notifications: notificationData,
-                        creators: sellerData,
-                    }}
-                />
-                <ProductAreaTwo
-                    data={{
-                        ...content["product-section"],
-                        products: productData,
-                    }}
-                />
-                <ServiceArea data={content["service-section"]} />
-                <CollectionArea
-                    data={{
-                        ...content["collection-section"],
-                        collections: collectionsData.slice(0, 4),
-                    }}
-                />*/}
 
             </main>
             <Footer />
